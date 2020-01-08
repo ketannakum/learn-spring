6af0a46d2471b666e4cfcf8a516a5e5f2dcb545c
Ketan Nakum <ketannakum@gmail.com>
2020-01-07 22:35:13 -0500

    <ketann> new method to take request param from input and print/ return
    Change-Id: I3028ee294aae69e1c3c27f50cd81231bf59f39db
    Change-Id: CI1

--- src/main/java/com/ex/learnspring/controllers/HelloController.java
+++ src/main/java/com/ex/learnspring/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.ex.learnspring.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,11 @@ public class HelloController {
 	public String sayHello()
 	{
 		return "Hello";
+	}
+	
+	@GetMapping("hello/{id}")
+	public String sayInput(@PathVariable("id") String id)
+	{
+		return "Hello " + id;
 	}
 }
